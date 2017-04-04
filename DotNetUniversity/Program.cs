5f7d4af1458da39f24197df9793f42520bc8ddf1
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-04 18:10:04 -0400

    --able to read the list of courses

--- DotNetUniversity/Program.cs
+++ DotNetUniversity/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,23 @@ namespace DotNetUniversity
         {
             //the database path 
             const string connectionString =
-                @"Server=localhost\SQLEXPRESS;Database=;Trusted_Connection=True;";
+                @"Server=localhost\SQLEXPRESS;Database=DotNetUniversity;Trusted_Connection=True;";
+
+            //open with using statement
+            using (var connection = new SqlConnection(connectionString))
+            {
+                
+                var sqlCommand = new SqlCommand(@"select * from Courses", connection);
+                connection.Open();
+                var reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["Title"] + "--" + reader["CourseNumber"]);
+                }
+                
+                connection.Close();
+
+            }
 
         }
     }
