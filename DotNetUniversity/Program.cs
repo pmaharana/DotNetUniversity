3af521febc518125cdf3e8b0e284d68bae145865
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-04 17:27:56 -0400

    --first commit

---
+++ DotNetUniversity/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetUniversity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the database path 
+            const string connectionString =
+                @"Server=localhost\SQLEXPRESS;Database=;Trusted_Connection=True;";
+
+        }
+    }
+}
